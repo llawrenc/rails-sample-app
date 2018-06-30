ce4fe630be48b5f7cc5e5df786cfb7b2f5bf9cdb
Michael Trojanek <michael.trojanek@relativkreativ.at>
2018-06-30 15:28:06 +0200

    Add product scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_085107) do
+ActiveRecord::Schema.define(version: 2018_06_30_132717) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_customers_on_name", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_products_on_name", unique: true
   end
 
 end
