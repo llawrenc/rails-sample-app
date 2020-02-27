0401fff1f02a6ede830b35a4a04c29fbb04486ce
Michael Trojanek <michael.trojanek@relativkreativ.at>
2020-02-27 08:51:33 +0100

    Add customer scaffold

---
+++ db/migrate/20200227073834_create_customers.rb
@@ -0,0 +1,10 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :customers, :name, unique: true
+  end
+end
